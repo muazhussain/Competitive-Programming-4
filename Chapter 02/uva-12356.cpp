904a0dd46c686f6a100974c7736982c3ef53b9a1
Muaz Hussain <muaz.self@gmail.com>
2023-06-15 20:35:08 +0600

    uva-12356 add

--- Chapter 02/uva-12356.cpp
+++ Chapter 02/uva-12356.cpp
@@ -10,7 +10,31 @@ using namespace std;
 #endif
 
 void solve() {
-	
+	int S, B;
+	while(cin >> S >> B && (S | B)) {
+		vector<int> L(S + 2), R(S + 1);
+		for(int i = 1; i <= S; ++i) {
+			L[i] = i - 1;
+			R[i] = i + 1;
+		}
+		while(B--) {
+			int f, t;
+			cin >> f >> t;
+			if(L[f] <= 0) {
+				cout << "* ";
+			} else {
+				cout << L[f] << " ";
+			}
+			if(R[t] > S) {
+				cout << "*\n";
+			} else {
+				cout << R[t] << "\n";
+			}
+			R[L[f]] = R[t];
+			L[R[t]] = L[f];
+		}
+		cout << "-\n";
+	}
 }
 
 int main() {
